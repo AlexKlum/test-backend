7a559cfbf615258b3cb9471aa1f6d91c0c3e2883
AlexKlum <alexanderklum@gmail.com>
2022-04-19 18:40:11 +0200

    La till mongoose och model mappen med filer. Kopplade även med mongodb

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+// === ALLA PROGRAM SOM BEHÖVS VARA MED FÖR ATT SIDAN SKA KUNNA KÖRAS ===
 const cors = require("cors")
 const express = require("express")
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser')
+
+
+// === KOPPLAR SIDAN TILL MIN MONGODB DATABAS===
+mongoose.connect("mongodb+srv://Klum:Hejsan123@cluster0.nfwnw.mongodb.net/test-db?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//const Thread = require("./model/threads");
+//const Reply = require("./model/replies");
+//const Like = require("./model/likes");
+const User = require("./model/users");
+const { response } = require("express");
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -10,12 +25,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(bodyParser.json());
 
+
+// === FÖRSTA SIDAN ===
+
 app.get("/", (request ,response)=>{
-    headers={"http_status":200, "cache-control": "no-cache"}
-    body={"status": "available"}
+    response.set("http_status",200)
+    response.set("cache-control", "no-cache")
     response.set('Content-Type', 'application/json');
+    body={"status": "available"}
     response.status(200).send(body)
+
+    User.create
 })
+
+// === THREADS ===
 
 app.get("/threads", (request, response) => {
     body={"threads":
@@ -67,6 +90,30 @@ app.post("/threads/:threadId/replies/:replyId/like", (request, response)=> {
 app.delete("/threads/:threadId/replies/:replyId/like", (request, response)=> {
     console.log(request.params)
     body={"threadId":request.params.threadId, "replyId": request.params.replyId}
+})
+
+// curl -X POST http://localhost:3001/users -H 'Content-Type: application/json' -d '{\"username\":\"nisse\",\"password\":\"password\"}'
+// Skapa User
+app.post("/users", (request, response) => {
+    console.log(request.body)
+    let user = new User(request.body)
+    user.save()
+    response.status(200).json(request.body)
+})
+
+// curl -X GET http://localhost:3001/users/625ed4fc79736a24c5e199d9 -H 'Content-Type: application/json'
+// Hämta User
+app.get("/users/:id", (request, response)=>{
+    console.log(request.params.id)
+    User.findById(request.params.id, (err, user)=>{
+        console.log(user)
+        if (err) throw error;
+        if (user) {
+            response.status(200).json(user)
+        } else {
+            response.status(404).send("Not found")
+        }
+    })
 })
 
 app.listen(PORT , ()=>{
